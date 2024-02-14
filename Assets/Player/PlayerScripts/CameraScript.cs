d8283366d74a6648bfe499aa200100ccb7ba9e25
Сергей Фомкин <144447986+SergeyFomkin5@users.noreply.github.com>
2024-02-14 18:29:17 +0300

    Initial commit

---
+++ Assets/Player/PlayerScripts/CameraScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CameraScript : MonoBehaviour
+{
+    //public float RotationSpeed;
+    //public Transform CameraTransform;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse X"), 0);
+
+
+    //    var newAngleX = CameraTransform.localEulerAngles.x - Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse Y");
+
+    //    CameraTransform.localEulerAngles = new Vector3(newAngleX, 0, 0);
+    //}
+
+    public float RotationSpeed;
+    public float minAngle;
+    public float maxAngle;
+
+    public Transform CameraAxisTransform;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse X"), 0);
+
+        var newAngleX = CameraAxisTransform.localEulerAngles.x - Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse Y");
+        if (newAngleX > 180)
+        {
+            newAngleX -= 360;
+        }
+        newAngleX = Mathf.Clamp(newAngleX, minAngle, maxAngle);
+
+        CameraAxisTransform.localEulerAngles = new Vector3(newAngleX, 0, 0);
+
+    }
+}
---
+++ Assets/Player/PlayerScripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    private Vector3 _moveVector;
+    private CharacterController _characterController;
+    private float _fallvelocity = 0;
+    public float gravity = 9.8f;
+    public float Speed;
+
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _moveVector = Vector3.zero;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            _moveVector += transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            _moveVector -= transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            _moveVector += transform.right;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            _moveVector -= transform.right;
+        }
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        _characterController.Move(_moveVector * Speed * Time.fixedDeltaTime);
+        _fallvelocity += gravity * Time.fixedDeltaTime;
+        _characterController.Move(Vector3.down * _fallvelocity * Time.fixedDeltaTime);
+        
+
+
+
+
+    }
+}
