8d3785c9dfc15695a0593e89699d0d7be7b8200e
Сергей Фомкин <144447986+SergeyFomkin5@users.noreply.github.com>
2024-02-22 12:09:49 +0300

    Delete PickPatrolPoints() from update

--- Assets/EnemyGoblin/EnemyScript.cs
+++ Assets/EnemyGoblin/EnemyScript.cs
@@ -29,7 +29,7 @@ public class EnemyScript : MonoBehaviour
     {
         NewPatrolPoint();
         CheckPlayerInView();
-        PickPatrolPoints();
+        
     }
 
     private void Componentlinks()
