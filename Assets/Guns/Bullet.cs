7f96494649f5919ff28d8d32050f6f123af7a673
Сергей Фомкин <144447986+SergeyFomkin5@users.noreply.github.com>
2024-02-25 23:34:20 +0300

    Create bullet script, bullet model and gun model

---
+++ Assets/Guns/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    Rigidbody rb;
+    public float ShotSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddRelativeForce(new Vector3(0, 0, ShotSpeed));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
