8a9dd2a0bb92648da89649bece0bce83ba87edd0
Сергей Фомкин <144447986+SergeyFomkin5@users.noreply.github.com>
2024-03-23 13:18:54 +0300

    Grenade prefab, explosion and throw

---
+++ Assets/Player/PlayerScripts/GreandeThrowing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreandeThrowing : MonoBehaviour
+{
+    public Rigidbody GrenadePrefab;
+    public Transform GrenadeTransform;
+
+    public float force;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            var greande = Instantiate(GrenadePrefab);
+            greande.transform.position = GrenadeTransform.position;
+            greande.GetComponent<Rigidbody>().AddForce(GrenadeTransform.forward * force);
+        }
+    }
+}
---
+++ Assets/Player/PlayerScripts/Grenade.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    public float delay = 3;
+    public GameObject ExplosionPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Invoke("Explosion", delay);
+    }
+
+    private void Explosion()
+    {
+        Destroy(gameObject);
+        var explosion = Instantiate(ExplosionPrefab);
+        explosion.transform.position = transform.position;
+    }
+}
